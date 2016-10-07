09f06a58ef67705944c9a2d4e56364f07be5e6b8
t00n <kelkununtel@hotmail.com>
2016-10-07 13:25:45 +0200

    [add] receive working, it seems

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,14 +1,13 @@
 #include "Timer.h"
 
-#define DEBUG
+#define DEBUG 1
 
 Timer t;
 
 int timer_receiver;
 int clock_receiver = 0;
-int state_sent = HIGH;
+int state_sent = LOW;
 int state_received;
-int synchronize_receiver = 0;
 int duration = 1000; // ms
 
 const int pin_emitter = 7;
@@ -18,7 +17,7 @@ const int pin_control_led = 13;
 void send_HIGH() {
 	digitalWrite(pin_emitter, HIGH);
 	state_sent = HIGH;
-	#ifdef DEBUG
+	#if DEBUG >= 2
 		Serial.println("Sending HIGH");
 	#endif
 }
@@ -26,7 +25,7 @@ void send_HIGH() {
 void send_LOW() {
 	digitalWrite(pin_emitter, LOW);
 	state_sent = LOW;
-	#ifdef DEBUG
+	#if DEBUG >= 2
 		Serial.println("Sending LOW");
 	#endif
 }
@@ -42,7 +41,7 @@ void toggle_send_state() {
 
 void send_random_bit() {
 	int bit_sent = random(2);
-	#ifdef DEBUG
+	#if DEBUG >= 1
 		Serial.print("Sending ");
 		Serial.println(bit_sent);
 	#endif
@@ -52,6 +51,9 @@ void send_random_bit() {
 }
 
 void send_clock_synchro() {
+	#if DEBUG >= 1
+		Serial.println("Sending synchronize");
+	#endif
 	toggle_send_state();
 }
 
@@ -63,15 +65,45 @@ void setup_send_random_bit() {
 	t.every(duration, send_random_bit);
 }
 
-// void receive_bit() {
-// 	bit_received = digitalRead(pin_receiver);
-// 	if (bit_received == bit_sent) {
-// 		Serial.print("OK");
-// 	}
-// 	else {
-// 		Serial.print("WTF");
-// 	}
-// }
+void receive() {
+	state_received = digitalRead(pin_receiver);
+	#if DEBUG >= 2
+		Serial.print("Received ");
+		Serial.print(state_received);
+	#endif
+}
+
+boolean receive_transition() {
+	int old_state = state_received;
+	receive();
+	return old_state != state_received;
+}
+
+void receive_clock_synchro() {
+	if (receive_transition()) {
+		#if DEBUG >= 1
+			Serial.println("Received clock synchro");
+		#endif
+		t.after(duration*1.05/2, receive_bit);
+	}
+	else {
+		t.after(duration*1.05/2, receive_clock_synchro);
+	}
+}
+
+void receive_bit() {
+	if (receive_transition()) {
+		#if DEBUG >= 1
+			Serial.println("Received 1");
+		#endif
+	}
+	else {
+		#if DEBUG >= 1
+			Serial.println("Received 0");
+		#endif
+	}
+	t.after(duration*1.05/2, receive_clock_synchro);
+}
 
 void setup() {
 	Serial.begin(9600);
@@ -80,6 +112,8 @@ void setup() {
 	pinMode(pin_control_led, OUTPUT); // control led
 	t.after(duration/2, setup_send_clock_synchro);
 	t.after(duration, setup_send_random_bit);
+	receive();
+	t.after(duration*1.05/2, receive_clock_synchro);
 }
 
 void loop() {
