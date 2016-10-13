b87a39b0c36ed6817620e08810919c1ceb4623cb
t00n <kelkununtel@hotmail.com>
2016-10-14 00:57:10 +0200

    [enh] use avr timer instead of lib

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,38 +1,33 @@
-#include "Timer.h"
-#include <assert.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 // if DEBUG == 0 -> no messages
 // if DEBUG == 1 -> high level messages
 // if DEBUG == 2 -> high + low level messages
 #define DEBUG 0
 
-Timer t;
 
+/* Global */
 int nb_good = 0;
 int nb_total = 0;
-int bit_sent;
+const uint32_t FREQUENCY = 250; // Hz
+
+/* Emitter */
+const int PIN_CALIBRATION = 6;
+const int PIN_EMITTER = 7;
+bool bit_sent;
+bool clock_sent = false;
 int state_sent = LOW;
-int state_received;
-int duration = 40; // ms
 
-const int pin_calibration = 6;
-const int pin_emitter = 7;
-const int pin_receiver = A0;
-const int pin_control_led = 13;
-
-void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
-    // transmit diagnostic informations through serial link. 
-    Serial.println(__func);
-    Serial.println(__file);
-    Serial.println(__lineno, DEC);
-    Serial.println(__sexp);
-    Serial.flush();
-    // abort program execution.
-    abort();
-}
+/* Receiver */
+const int PIN_RECEIVER = A0;
+const int PIN_CONTROL_LED = 13;
+bool clock_received = false;
+int state_received = LOW;
+uint32_t last_t;
 
 void send_HIGH() {
-	digitalWrite(pin_emitter, HIGH);
+	digitalWrite(PIN_EMITTER, HIGH);
 	state_sent = HIGH;
 	#if DEBUG >= 2
 		Serial.println("Sending HIGH");
@@ -40,7 +35,7 @@ void send_HIGH() {
 }
 
 void send_LOW() {
-	digitalWrite(pin_emitter, LOW);
+	digitalWrite(PIN_EMITTER, LOW);
 	state_sent = LOW;
 	#if DEBUG >= 2
 		Serial.println("Sending LOW");
@@ -75,41 +70,19 @@ void send_clock_synchro() {
 	toggle_send_state();
 }
 
-void setup_send_clock_synchro() {
-	t.every(duration, send_clock_synchro);
-}
-
-void setup_send_random_bit() {
-	t.every(duration, send_random_bit);
-}
-
 void receive() {
-	int read = analogRead(pin_receiver);
-	state_received = read > 60;
-	// Serial.println(read);
+	state_received = analogRead(PIN_RECEIVER) > 60;
+	digitalWrite(PIN_CONTROL_LED, state_received ? HIGH : LOW);
 	#if DEBUG >= 2
-		digitalWrite(pin_control_led, state_received ? HIGH : LOW);
 		Serial.print("Received ");
 		Serial.println(state_received);
 	#endif
 }
 
-boolean receive_transition() {
+bool receive_transition() {
 	int old_state = state_received;
 	receive();
 	return old_state != state_received;
-}
-
-void receive_clock_synchro() {
-	if (receive_transition()) {
-		#if DEBUG >= 1
-			Serial.println("Received clock synchro");
-		#endif
-		t.after(duration/2, receive_bit);
-	}
-	else {
-		t.after(duration/2, receive_clock_synchro);
-	}
 }
 
 void receive_bit() {
@@ -121,23 +94,56 @@ void receive_bit() {
 	if (bit_received == bit_sent) {
 		nb_good += 1;
 	}
-	Serial.print("Good : ");
 	Serial.println((float)nb_good/(float)nb_total);
-	t.after(duration/2, receive_clock_synchro);
 }
+
+uint32_t timer1_counter;
+uint32_t timer2_counter;
 
 void setup() {
 	Serial.begin(9600);
-	pinMode(pin_calibration, OUTPUT); // always on for calibration
-	digitalWrite(pin_calibration, HIGH);
-	pinMode(pin_emitter, OUTPUT); // pseudo-laser
-	pinMode(pin_control_led, OUTPUT); // control led
-	t.after(duration/2, setup_send_clock_synchro);
-	t.after(duration, setup_send_random_bit);
-	receive();
-	t.after(duration*3/4, receive_clock_synchro);
+	pinMode(PIN_CALIBRATION, OUTPUT); // always on for calibration
+	digitalWrite(PIN_CALIBRATION, HIGH);
+	pinMode(PIN_EMITTER, OUTPUT); // laser
+	pinMode(PIN_CONTROL_LED, OUTPUT); // control led
+
+	noInterrupts();
+
+	// TIMER1 : emitter
+	TCCR1A = 0;
+	TCCR1B = 0;
+	timer1_counter = 65536 - 62500/FREQUENCY;
+	TCNT1 = timer1_counter;   // preload timer
+	TCCR1B |= (1 << CS12);    // prescaler : 256
+	TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+
+	interrupts();             // enable all interrupts
+
+}
+
+ISR(TIMER1_OVF_vect)        // interrupt service routine 
+{
+	TCNT1 = timer1_counter;   // preload timer
+	if (! clock_sent) {
+		send_clock_synchro();
+	}
+	else {
+		send_random_bit();
+	}
+	clock_sent ^= 1;
 }
 
 void loop() {
-	t.update();
+	if (! clock_received) {
+		if (receive_transition()) {
+			clock_received = true;
+			last_t = micros();
+		}
+	}
+	else {
+		if (micros() - last_t >= 1500000/(FREQUENCY)) {
+			receive_bit();
+			clock_received = false;
+		}
+	}
 }
